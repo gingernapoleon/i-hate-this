97e402e19ad747f14c78d79cbbe626d4aaddc6c9
Michael Simms <81196688+gingernapoleon@users.noreply.github.com>
2021-07-06 18:50:17 -0700

    created layout
    Layout
    Bootstrap
    react-nimated-weather
    Axios
    API
    Time
    Forecast
    Temperature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-  Weather Forecast One Day
+  <div className="App">
+  <div className="container">
+    <Weather defaultCity="New York"/>
+    <footer>
+      This project was coded by{" "}
+      <a href="https://www.linkedin.com/in/napoleon-6b89b756/" 
+      target="_blank" 
+      rel="noreferrer">
+      Ginger Napoleon
+      </a>{" "} 
+      and is{" "}  
+      <a href="https://github.com/gingernapoleon/i-hate-this" 
+      target="_blank" 
+      rel="noreferrer"> 
+      open-sourced on Github
+     </a>{" "}
+          and{" "}
+          <a
+            href="https://fervent-shannon-66b4ff.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+    </footer>
+    </div>
     </div>
   );
 }
-
-export default App;
